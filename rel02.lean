4e23f5d81822c3d8f81ea0c5f7ce2ed43e9359f4
Caprusia <boris.fortuin@gmail.com>
2026-05-10 14:02:32 +0200

    1 to 3

--- rel02.lean
+++ rel02.lean
@@ -3,6 +3,10 @@ namespace rel02
 variable {a b : Prop}
 
 theorem rel02 : (∀ x, a = a → a = x) → b = a := by
-  sorry
+intro h1
+apply Eq.symm
+apply h1
+rfl
+
 
 end rel02
--- rel03.lean
+++ rel03.lean
@@ -3,6 +3,16 @@ namespace rel03
 variable {a b c : Prop} {P : Prop → Prop}
 
 theorem rel03 : a = c ∨ b = c → P a → P b → P c := by
-  sorry
+intro h1
+intro hpa
+intro hpb
+cases h1 with
+| inl hac =>
+  rw [<-hac]
+  exact hpa
+| inr hbc =>
+  rw [<-hbc]
+  exact hpb
+  
 
 end rel03
