2424c29559d71355cda5e3e09a05ab0a551de4de
Caprusia <boris.fortuin@gmail.com>
2026-05-11 16:59:47 +0200

    worked on 4

--- rel04.lean
+++ rel04.lean
@@ -1,10 +1,18 @@
 namespace rel04
+--test
 
 variable {R : Prop → Prop → Prop}
 
 theorem rel04 (irrefl : ∀ x, ¬ R x x)
               (trans : ∀ x y z, R x y → R y z → R x z) :
               ∀ x y, R x y → ¬ R y x := by
+       intro a
+       intro b
+       intro hrab
+       intro hrba
+       apply irrefl
+       apply trans
+
   sorry
 
 end rel04
--- rel05.lean
+++ rel05.lean
@@ -1,5 +1,5 @@
 namespace rel05
-
+--test
 variable {P : Prop → Prop}
 
 theorem rel05 : (∃ x, P x) ∧ (∀ x y, P x ∧ P y →  x = y) → ∃ x, P x ∧ ∃ y, P y → y = x := by
